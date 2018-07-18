f180897e967529a4563e8479e71e7d2febc6e1c2
Neriberto C.Prado <neriberto@gmail.com>
2018-07-18 19:21:27 +0000

    Updating code

--- src/teste.h
+++ src/teste.h
@@ -1,9 +1,9 @@
 #include <string>
 
-/*
-* @brief Pega a versão do sistema operacional
-* @param OSVersion variável de saída que irá conter a versão do sistema operacional
-*/
+/**
+ *  @brief Pega a versão do sistema operacional
+ * @param OSVersion variável de saída que irá conter a versão do sistema operacional
+ */
 void GetOperationalSystemVersion(std::string &OSVersion);
 
 void FunctionWithoutDoc();
