43d36f92540a10dfae857dc95293783415d2aa11
Neriberto C.Prado <neriberto@gmail.com>
2018-07-18 17:12:55 +0000

    Added source code

---
+++ src/teste.h
@@ -0,0 +1,7 @@
+#include <string>
+
+/*
+* @brief Pega a versão do sistema operacional
+* @param OSVersion variável de saída que irá conter a versão do sistema operacional
+*/
+void GetOperationalSystemVersion(std::string &OSVersion);
